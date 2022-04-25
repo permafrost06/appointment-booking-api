37b6259961c30412f0b7d199755db510960ea485
permafrost06 <zahin06@gmail.com>
2022-04-26 00:14:11 +0600

    first commit

---
+++ src/data.ts
@@ -0,0 +1,33 @@
+interface Todo {
+  id: number;
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+export const todos: Todo[] = [
+  {
+    id: 1,
+    title: "Coding in Javascript",
+    description: "Working with functions in JavaScript",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Cooking Supper",
+    description: "Preparing rice and chicken",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Taking a walk",
+    description: "Easy time at the park",
+    completed: false,
+  },
+  {
+    id: 14,
+    title: "Watching Netflix",
+    description: "Enjoying the new premiered series",
+    completed: false,
+  },
+];

b0f4f3338532b8d03fe01a3481a61713da495148
permafrost06 <zahin06@gmail.com>
2022-04-26 17:08:12 +0600

    added AdminController

---
+++ src/controllers/AdminController.ts
@@ -0,0 +1,32 @@
+import * as jwt from "jsonwebtoken";
+
+export class AdminController {
+  jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  signIn(username: string, password: string): string {
+    if (username === "admin" && password === "password") {
+      const data = {
+        userLevel: "admin",
+      };
+
+      return jwt.sign(data, this.jwtSecretKey);
+    }
+  }
+
+  isAdmin(token: string): boolean {
+    try {
+      interface Dec {
+        userLevel: string;
+        iat: number;
+      }
+
+      const decoded = jwt.verify(token.split(" ")[1], this.jwtSecretKey) as Dec;
+
+      if (decoded.userLevel === "admin") return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+    return false;
+  }
+}
